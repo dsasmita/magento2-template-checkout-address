74d960340da9a1ece61766c81b3b69f824d7512f
dsasmita <dangs.work@gmail.com>
2018-08-04 19:38:38 +0700

    Create helper

---
+++ Helper/Data.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Lingble\CheckoutAddressTemplate\Helper;
+
+/**
+ * Helper Data.
+ */
+class Data extends \Magento\Framework\App\Helper\AbstractHelper
+{
+
+	/**
+     * get address template
+     *
+     * @return string
+     */
+
+	public function getAddressHtmlFormat(){
+		return $this->scopeConfig->getValue('customer/address_templates_checkout/html', 
+						\Magento\Store\Model\ScopeInterface::SCOPE_STORE);
+	}
+}
